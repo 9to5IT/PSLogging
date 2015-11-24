da2d4329f880e49b854767e60101b35074d2aad9
Wojciech Sciesinski <wojciech@sciesinski.net>
2015-11-24 08:18:17 +0100

    The en-US XML help file created, the functions updated to the Module version 3.0.0

--- Module/PSLogging/PSLogging.psm1
+++ Module/PSLogging/PSLogging.psm1
@@ -1,7 +1,7 @@
 ###
 # Author: Luca Sturlese
 # URL: http://9to5IT.com
-# Author: Wojciech Sciesinski
+# Author: Wojciech Sciesinski wojciech[at]sciesinski[dot]net
 # URL: https://www.linkedin.com/in/sciesinskiwojciech
 ###
 
@@ -10,62 +10,48 @@ Set-StrictMode -Version Latest
 
 Function Start-Log {
   <#
-  .SYNOPSIS
+    .SYNOPSIS
     Creates a new log file
 
-  .DESCRIPTION
+    .DESCRIPTION
     Creates a log file with the path and name specified in the parameters. Checks if log file exists, and if it does deletes it and creates a new one.
     Once created, writes initial logging data
 
-  .PARAMETER LogPath
+    .PARAMETER LogPath
     Mandatory. Path of where log is to be created. Example: C:\Windows\Temp
 
-  .PARAMETER LogName
+    .PARAMETER LogName
     Mandatory. Name of log file to be created. Example: Test_Script.log
 
-  .PARAMETER ScriptVersion
+    .PARAMETER ScriptVersion
     Mandatory. Version of the running script which will be written in the log. Example: 1.5
 
-  .PARAMETER ToScreen
+    .PARAMETER ToScreen
     Optional. When parameter specified will display the content to screen as well as write to log file. This provides an additional
     another option to write content to screen as opposed to using debug mode.
 
-  .INPUTS
+    .INPUTS
     Parameters above
 
-  .OUTPUTS
+    .OUTPUTS
     Log file created
 
-  .NOTES
-    Version:        1.0
-    Author:         Luca Sturlese
-    Creation Date:  10/05/12
-    Purpose/Change: Initial function development.
+    .NOTES
+    Current version:  1.5
+	
+	Version history: github.com/9to5IT/PSLogging/VERSIONS.MD
+	
+	Authors: 
+	- Luca Sturlese 
+	- Wojciech Sciesinski wojciech[at]sciesinski[dot]net
+	
+    .LINK
+	https://github.com/9to5IT/PSLogging	
 
-    Version:        1.1
-    Author:         Luca Sturlese
-    Creation Date:  19/05/12
-    Purpose/Change: Added debug mode support.
-
-    Version:        1.2
-    Author:         Luca Sturlese
-    Creation Date:  02/09/15
-    Purpose/Change: Changed function name to use approved PowerShell Verbs. Improved help documentation.
-
-    Version:        1.3
-    Author:         Luca Sturlese
-    Creation Date:  07/09/15
-    Purpose/Change: Resolved issue with New-Item cmdlet. No longer creates error. Tested - all ok.
-
-    Version:        1.4
-    Author:         Luca Sturlese
-    Creation Date:  12/09/15
-    Purpose/Change: Added -ToScreen parameter which will display content to screen as well as write to the log file.
-
-  .LINK
+    .LINK
     http://9to5IT.com/powershell-logging-v2-easily-create-log-files
 
-  .EXAMPLE
+    .EXAMPLE
     Start-Log -LogPath "C:\Windows\Temp" -LogName "Test_Script.log" -ScriptVersion "1.5"
 
     Creates a new log file with the file path of C:\Windows\Temp\Test_Script.log. Initialises the log file with
@@ -129,44 +115,59 @@ Function Start-Log {
 }
 
 Function Write-LogEntry {
-  <#
-  .SYNOPSIS
+<#
+    .SYNOPSIS
     Writes a message to specified log file
 
-  .DESCRIPTION
+    .DESCRIPTION
     Appends a new message to the specified log file
 
-  .PARAMETER LogPath
+    .PARAMETER LogPath
     Mandatory. Full path of the log file you want to write to. Example: C:\Windows\Temp\Test_Script.log
     
-  .PARAMETER MessageType
-    Mandatory. Allowed message types: INFO, WARNING, ERROR, CRITICAL, START, STOP
+    .PARAMETER MessageType
+    Mandatory. Allowed message types: INFO, WARNING, ERROR, CRITICAL, START, STOP, SUCCESS, FAILURE
 
-  .PARAMETER Message
+    .PARAMETER Message
     Mandatory. The string that you want to write to the log
 
-  .PARAMETER TimeStamp
+    .PARAMETER TimeStamp
     Optional. When parameter specified will append the current date and time to the end of the line. Useful for knowing
     when a task started and stopped.
+    
+    .PARAMETER EntryDateTime
+    Optional. By default a current date and time is used but it is possible provide any other correct date/time.
+    
+    .PARAMETER ConvertTimeToUTF
+    # Need be filled
 
-  .PARAMETER ToScreen
+    .PARAMETER ToScreen
     Optional. When parameter specified will display the content to screen as well as write to log file. This provides an additional
     another option to write content to screen as opposed to using debug mode.
+    
+    .PARAMETER
 
-  .INPUTS
+    .INPUTS
     Parameters above
 
-  .OUTPUTS
-    None
+    .OUTPUTS
+    None or String
 
-  .NOTES
+    .NOTES
     
-    Version:        2.0
-    Author:         Wojciech Sciesinski, wojciech[at]sciesinski[dot]net
-    Purpose/Change: Initial function development.
-    Creation Date:  25/10/2015
+	Current version:  1.2.0
+	
+	Authors: 
+	- Wojciech Sciesinski wojciech[at]sciesinski[dot]net
+	
+	Version history: github.com/9to5IT/PSLogging/VERSIONS.MD
     
     Inspired and partially based on PSLogging module authored by Luca Sturlese - https://github.com/9to5IT/PSLogging
+    
+    TODO
+    Updated examples - add additional with new implemented parameters
+    Implement converting day/time to UTF
+    Output with colors (?) - Write-Host except Write-Output need to be used
     
     .LINK
     http://9to5IT.com/powershell-logging-v2-easily-create-log-files
@@ -178,25 +179,33 @@ Function Write-LogEntry {
     https://www.linkedin.com/in/sciesinskiwojciech
     
 
-  .EXAMPLE
+    .EXAMPLE
     Write-LogEntry -LogPath "C:\Windows\Temp\Test_Script.log" -MessageType CRITICAL -Message "This is a new line which I am appending to the end of the log file."
 
     Writes a new critical log message to a new line in the specified log file.
+    
   #>
     
     [CmdletBinding()]
     Param (
-        [Parameter(Mandatory = $true, Position = 0)]
+        [Parameter(Mandatory = $false, ParameterSetName = "WriteToFile")]
+        [Switch]$ToFile,
+        [Parameter(Mandatory = $false, ParameterSetName = "WriteToFile")]
         [string]$LogPath,
-        [Parameter(Mandatory = $true, Position = 1, HelpMessage = "Allowed values: INFO, WARNING, ERROR, CRITICAL, START, STOP")]
-        [ValidateSet("INFO", "WARNING", "ERROR", "CRITICAL", "START", "STOP")]
+        [Parameter(Mandatory = $true, HelpMessage = "Allowed values: INFO, WARNING, ERROR, CRITICAL, START, STOP, SUCCESS, FAILURE")]
+        [ValidateSet("INFO", "WARNING", "ERROR", "CRITICAL", "START", "STOP", "SUCCESS", "FAILURE")]
         [String]$MessageType,
-        [Parameter(Mandatory = $true, Position = 2, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [Alias("EventMessage", "EntryMessage")]
         [string]$Message,
-        [Parameter(Mandatory = $false, Position = 3)]
+        [Parameter(Mandatory = $false)]
         [switch]$TimeStamp,
-        [Parameter(Mandatory = $false, Position = 4)]
+        [Parameter(Mandatory = $false)]
+        [Alias("EventDateTime", "EntryDate", "MessageDate")]
+        [DateTime]$EntryDateTime = $([DateTime]::Now),
+        [Parameter(Mandatory = $false)]
         [switch]$ToScreen
+        
     )
     
     
@@ -209,12 +218,12 @@ Function Write-LogEntry {
         #A padding used to allign columns in output file
         [String]$Padding = " " * $(10 - $CapitalizedMessageType.Length)
         
-        #Add TimeStamp to message if specified
+        #Add TimeStamp to message if required
         If ($TimeStamp -eq $True) {
             
-            [String]$MessageToFile = "[{0}][{1}{2}][{3}]" -f $([DateTime]::Now), $CapitalizedMessageType, $Padding, $Message
+            [String]$MessageToFile = "[{0}][{1}{2}][{3}]" -f $EntryDateTime, $CapitalizedMessageType, $Padding, $Message
             
-            [String]$MessageToScreen = "[{0}] {1}: {2}" -f $([DateTime]::Now), $CapitalizedMessageType, $Message
+            [String]$MessageToScreen = "[{0}] {1}: {2}" -f $EntryDateTime, $CapitalizedMessageType, $Message
             
         }
         Else {
@@ -225,7 +234,12 @@ Function Write-LogEntry {
         }
         
         #Write Content to Log
-        Add-Content -Path $LogPath -Value $MessageToFile
+        
+        If ($ToFile -eq $true) {
+            
+            Add-Content -Path $LogPath -Value $MessageToFile
+            
+        }
         
         #Write to screen for debug mode
         Write-Debug $MessageToScreen
@@ -236,6 +250,7 @@ Function Write-LogEntry {
             Write-Output $MessageToScreen
             
         }
+        
     }
 }
 
@@ -268,41 +283,17 @@ Function Write-LogInfo {
     None
     
     .NOTES
-    Version:        1.0
-    Author:         Luca Sturlese
-    Creation Date:  10/05/12
-    Purpose/Change: Initial function development.
+	Current version:  2.1
+	
+	Version history: github.com/9to5IT/PSLogging/VERSIONS.MD
+	
+	Authors: 
+	- Luca Sturlese 
+	- Wojciech Sciesinski wojciech[at]sciesinski[dot]net
     
-    Version:        1.1
-    Author:         Luca Sturlese
-    Creation Date:  19/05/12
-    Purpose/Change: Added debug mode support.
-    
-    Version:        1.2
-    Author:         Luca Sturlese
-    Creation Date:  02/09/15
-    Purpose/Change: Changed function name to use approved PowerShell Verbs. Improved help documentation.
-    
-    Version:        1.3
-    Author:         Luca Sturlese
-    Creation Date:  02/09/15
-    Purpose/Change: Changed parameter name from LineValue to Message to improve consistency across functions.
-    
-    Version:        1.4
-    Author:         Luca Sturlese
-    Creation Date:  12/09/15
-    Purpose/Change: Added -TimeStamp parameter which append a timestamp to the end of the line. Useful for knowing when a task started and stopped.
-    
-    Version:        1.5
-    Author:         Luca Sturlese
-    Creation Date:  12/09/15
-    Purpose/Change: Added -ToScreen parameter which will display content to screen as well as write to the log file.
-    
-    Version:        2.0
-    Author:         Wojciech Sciesinski, wojciech[at]sciesinski[dot]net
-    Purpose/Change: Changed format of logs displayed and saved to file, function changed to be wrapper for Write-LogEntry
-    Creation Date:  25/10/2015
-    
+	.LINK
+	https://github.com/9to5IT/PSLogging	
+
     .LINK
     http://9to5IT.com/powershell-logging-v2-easily-create-log-files
   
@@ -360,26 +351,16 @@ Function Write-LogWarning {
     None
 
     .NOTES
-    Version:        1.0
-    Author:         Luca Sturlese
-    Creation Date:  02/09/15
-    Purpose/Change: Initial function development.
-
-    Version:        1.1
-    Author:         Luca Sturlese
-    Creation Date:  12/09/15
-    Purpose/Change: Added -TimeStamp parameter which append a timestamp to the end of the line. Useful for knowing when a task started and stopped.
-
-    Version:        1.2
-    Author:         Luca Sturlese
-    Creation Date:  12/09/15
-    Purpose/Change: Added -ToScreen parameter which will display content to screen as well as write to the log file.
+	Current version:  2.1
+	
+	Version history: github.com/9to5IT/PSLogging/VERSIONS.MD
+	
+	Authors: 
+	- Luca Sturlese 
+	- Wojciech Sciesinski wojciech[at]sciesinski[dot]net
     
-    Version:        2.0
-    Author:         Wojciech Sciesinski, wojciech[at]sciesinski[dot]net
-    Purpose/Change: Changed format of logs displayed and saved to file, function changed to be wrapper for Write-LogEntry
-    Creation Date:  25/10/2015
-    
+	.LINK
+	https://github.com/9to5IT/PSLogging	
 
     .LINK
     http://9to5IT.com/powershell-logging-v2-easily-create-log-files
@@ -440,51 +421,17 @@ Function Write-LogError {
     .OUTPUTS
     None
 
-    .NOTES
-    Version:        1.0
-    Author:         Luca Sturlese
-    Creation Date:  10/05/12
-    Purpose/Change: Initial function development.
+	.NOTES
+    Current version:  2.1
+	
+	Version history: github.com/9to5IT/PSLogging/VERSIONS.MD
+	
+	Authors: 
+	- Luca Sturlese 
+	- Wojciech Sciesinski wojciech[at]sciesinski[dot]net
 
-    Version:        1.1
-    Author:         Luca Sturlese
-    Creation Date:  19/05/12
-    Purpose/Change: Added debug mode support. Added -ExitGracefully parameter functionality.
-
-    Version:        1.2
-    Author:         Luca Sturlese
-    Creation Date:  02/09/15
-    Purpose/Change: Changed function name to use approved PowerShell Verbs. Improved help documentation.
-
-    Version:        1.3
-    Author:         Luca Sturlese
-    Creation Date:  02/09/15
-    Purpose/Change: Changed parameter name from ErrorDesc to Message to improve consistency across functions.
-
-    Version:        1.4
-    Author:         Luca Sturlese
-    Creation Date:  03/09/15
-    Purpose/Change: Improved readability and cleaniness of error writing.
-
-    Version:        1.5
-    Author:         Luca Sturlese
-    Creation Date:  12/09/15
-    Purpose/Change: Changed -ExitGracefully parameter to switch type so no longer need to specify $True or $False (see example for info).
-
-    Version:        1.6
-    Author:         Luca Sturlese
-    Creation Date:  12/09/15
-    Purpose/Change: Added -TimeStamp parameter which append a timestamp to the end of the line. Useful for knowing when a task started and stopped.
-
-    Version:        1.7
-    Author:         Luca Sturlese
-    Creation Date:  12/09/15
-    Purpose/Change: Added -ToScreen parameter which will display content to screen as well as write to the log file.
-    
-    Version:        2.0
-    Author:         Wojciech Sciesinski, wojciech[at]sciesinski[dot]net
-    Purpose/Change: Changed format of logs displayed and saved to file, function changed to be wrapper for Write-LogEntry
-    Creation Date:  25/10/2015
+    .LINK
+	https://github.com/9to5IT/PSLogging	
 
     .LINK
     http://9to5IT.com/powershell-logging-v2-easily-create-log-files
@@ -561,35 +508,16 @@ Function Stop-Log {
     None
 
     .NOTES
-    Version:        1.0
-    Author:         Luca Sturlese
-    Creation Date:  10/05/12
-    Purpose/Change: Initial function development.
-
-    Version:        1.1
-    Author:         Luca Sturlese
-    Creation Date:  19/05/12
-    Purpose/Change: Added debug mode support.
-
-    Version:        1.2
-    Author:         Luca Sturlese
-    Creation Date:  01/08/12
-    Purpose/Change: Added option to not exit calling script if required (via optional parameter).
-
-    Version:        1.3
-    Author:         Luca Sturlese
-    Creation Date:  02/09/15
-    Purpose/Change: Changed function name to use approved PowerShell Verbs. Improved help documentation.
-
-    Version:        1.4
-    Author:         Luca Sturlese
-    Creation Date:  12/09/15
-    Purpose/Change: Changed -NoExit parameter to switch type so no longer need to specify $True or $False (see example for info).
-
-    Version:        1.5
-    Author:         Luca Sturlese
-    Creation Date:  12/09/15
-    Purpose/Change: Added -ToScreen parameter which will display content to screen as well as write to the log file.
+    Current version:  1.7
+	
+	Version history: github.com/9to5IT/PSLogging/VERSIONS.MD
+	
+	Authors: 
+	- Luca Sturlese 
+	- Wojciech Sciesinski wojciech[at]sciesinski[dot]net
+ 
+    .LINK
+	https://github.com/9to5IT/PSLogging	
 
     .LINK
     http://9to5IT.com/powershell-logging-v2-easily-create-log-files
@@ -676,20 +604,16 @@ Function Send-Log {
     Email sent to the list of addresses specified
 
     .NOTES
-    Version:        1.0
-    Author:         Luca Sturlese
-    Creation Date:  05.10.12
-    Purpose/Change: Initial function development.
+    Current version:  1.3
+	
+	Version history: github.com/9to5IT/PSLogging/VERSIONS.MD
+	
+	Authors: 
+	- Luca Sturlese 
+	- Wojciech Sciesinski wojciech[at]sciesinski[dot]net
 
-    Version:        1.1
-    Author:         Luca Sturlese
-    Creation Date:  02/09/15
-    Purpose/Change: Changed function name to use approved PowerShell Verbs. Improved help documentation.
-
-    Version:        1.2
-    Author:         Luca Sturlese
-    Creation Date:  02/09/15
-    Purpose/Change: Added SMTPServer parameter to pass SMTP server as oppposed to having to set it in the function manually.
+    .LINK
+	https://github.com/9to5IT/PSLogging	
 
     .LINK
     http://9to5IT.com/powershell-logging-v2-easily-create-log-files
